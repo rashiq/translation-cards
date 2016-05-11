b10baca4ffe9014efb6aa08b2024a9b63f08080e
Austin Garrard <agarrard@thoughtworks.com>
2016-05-11 14:15:11 -0500

    Austin/Nat #155 UI Fixes for new deck creation screens

--- app/src/test/java/org/mercycorps/translationcards/activity/addDeck/AuthorAndLockActivityTest.java
+++ app/src/test/java/org/mercycorps/translationcards/activity/addDeck/AuthorAndLockActivityTest.java
@@ -1,6 +1,7 @@
 package org.mercycorps.translationcards.activity.addDeck;
 
 import android.app.Activity;
+import android.widget.ImageView;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,8 +13,10 @@ import org.robolectric.annotation.Config;
 
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
+import static org.mercycorps.translationcards.util.TestAddDeckActivityHelper.createActivityToTest;
 import static org.mercycorps.translationcards.util.TestAddDeckActivityHelper.createActivityToTestWithContext;
 import static org.mercycorps.translationcards.util.TestAddTranslationCardActivityHelper.click;
+import static org.mercycorps.translationcards.util.TestAddTranslationCardActivityHelper.findImageView;
 import static org.robolectric.Shadows.shadowOf;
 
 @Config(constants = BuildConfig.class, sdk = 21)
@@ -34,5 +37,12 @@ public class AuthorAndLockActivityTest  {
         Activity activity = createActivityToTestWithContext(AuthorAndLockActivity.class, newDeckContext);
         click(activity, R.id.deck_author_and_lock_back);
         assertEquals(EnterDeckDestinationLanguagesActivity.class.getName(), shadowOf(activity).getNextStartedActivity().getComponent().getClassName());
+    }
+
+    @Test
+    public void shouldInflateAuthorAndLockImageWhenActivityIsCreated() {
+        Activity activity = createActivityToTest(AuthorAndLockActivity.class);
+        ImageView imageView = findImageView(activity, R.id.deck_author_and_lock_image);
+        assertEquals(R.drawable.enter_phrase_image, shadowOf(imageView.getDrawable()).getCreatedFromResId());
     }
 }
